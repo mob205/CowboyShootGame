522efd6e8718d0f3aaf10c6fccfd08d0dc7e9164
Donny M <Mobslayer205@gmail.com>
2020-03-10 20:37:04 -0400

    Initial commit. Player movement and animations. Gun follows mouse.

---
+++ Assets/GunController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    [SerializeField] Transform pivot;
+    Camera mainCamera;
+    PlayerController player;
+
+    const float Left = 180f;
+    const float Right = 0f;
+
+    float startingY, startingZ;
+
+    void Start()
+    {
+        player = GetComponentInParent<PlayerController>();
+        mainCamera = Camera.main;
+        startingY = transform.eulerAngles.y;
+        startingZ = transform.eulerAngles.z;
+    }
+
+    void Update()
+    {
+        RotatePivot();
+        RotateGun();
+    }
+    void RotatePivot()
+    {
+        var mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        var pivotPos = pivot.position;
+
+        mousePos.x -= pivotPos.x;
+        mousePos.y -= pivotPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        pivot.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+    void RotateGun()
+    {
+        if (Input.mousePosition.x < mainCamera.WorldToScreenPoint(player.transform.position).x)
+        {
+            transform.localRotation = Quaternion.Euler(new Vector3(180, startingY, startingZ));
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(new Vector3(0, startingY, startingZ));
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public const float Left = 0;
+    public const float Right = 1;
+
+    [SerializeField] float playerSpeed;
+
+    Camera mainCamera;
+    Rigidbody2D rb;
+    Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        ProcessMovement();
+        ProcessAnimations();
+    }
+    void ProcessMovement()
+    {
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        rb.MovePosition(transform.position + (movement * playerSpeed * Time.deltaTime));
+        animator.SetBool("IsMoving", (movement.magnitude > 0));
+    }
+    void ProcessAnimations()
+    {
+        
+        if (Input.mousePosition.x < mainCamera.WorldToScreenPoint(transform.position).x)
+        {
+            animator.SetFloat("Direction", Left);
+        }
+        else
+        {
+            animator.SetFloat("Direction", Right);
+        }
+    }
+}
