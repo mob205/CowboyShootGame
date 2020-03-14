d605885fe57aa54be9566d4b9fedc741bee61647
Donny M <Mobslayer205@gmail.com>
2020-03-14 13:55:45 -0400

    Added fences along the borders of the area. Enemies now randomly spawn on the sides of the map. Enemies 'explode' on death, releasing particles and damaging nearby enemies.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Enemy : MonoBehaviour
 {
+    [Header("General")]
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] LayerMask explosionLayers;
+
+    [Header("Stats")]
     [SerializeField] float baseHealth;
     [SerializeField] float moveSpeed;
+    [SerializeField] float deathExplosionRadius;
+    [SerializeField] float explosionDamage;
+
+
 
     const float Left = 0;
     const float Right = 1;
@@ -29,7 +38,10 @@ public class Enemy : MonoBehaviour
     }
     virtual protected void Move()
     {
-        rb.MovePosition(Vector2.Lerp(transform.position, PlayerController.instance.transform.position, moveSpeed * Time.deltaTime));
+        var direction = (PlayerController.instance.transform.position - transform.position).normalized;
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+        
+        //rb.MovePosition(Vector2.Lerp(transform.position, PlayerController.instance.transform.position, moveSpeed * Time.deltaTime));
         
         if(PlayerController.instance.transform.position.x < transform.position.x)
         {
@@ -39,5 +51,40 @@ public class Enemy : MonoBehaviour
         {
             anim.SetFloat("Direction", Right);
         }
+    }
+    virtual protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            var bullet = collision.GetComponent<Bullet>();
+            Damage(bullet.damage);
+        }
+    }
+    virtual public void Damage(float damage)
+    {
+        Debug.Log("damaged for " + damage);
+        health -= damage;
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+    virtual protected void Die()
+    {
+        var hitEnemies = Physics2D.OverlapCircleAll(transform.position, deathExplosionRadius, explosionLayers);
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().Damage(explosionDamage);
+        }
+
+        EnemySpawner.instance.AlertDeath();
+        deathParticles.transform.parent = null;
+        deathParticles.Play();
+        Destroy(deathParticles.gameObject, deathParticles.main.duration);
+        Destroy(gameObject);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, deathExplosionRadius);
     }
 }
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [HideInInspector] public static EnemySpawner instance;
+
+    [SerializeField] BoxCollider2D[] spawnAreas;
+    [SerializeField] float minSpawnTime = 0.5f;
+    [SerializeField] float maxSpawnTime = 3f;
+    [SerializeField] GameObject enemyPrefab;
+
+    int enemyCount;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            
+            Instantiate(enemyPrefab, GetRandomPosition(), Quaternion.identity);
+            enemyCount++;
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+        }
+    }
+    private Vector2 GetRandomPosition()
+    {
+        var box = spawnAreas[Random.Range(0, spawnAreas.Length)];
+        Vector2 randomPosition = new Vector2(Random.Range(-box.size.x / 2, box.size.x), Random.Range(-box.size.y / 2, box.size.y / 2));
+        return (Vector2)box.transform.position + randomPosition;
+    }
+    public void AlertDeath()
+    {
+        enemyCount--;
+    }
+}
